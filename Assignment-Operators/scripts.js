8594e59f847c98b45a2f2be18eb32dbcbcd40fc3
bklanuradha <92729@fhss.sjp.ac.lk>
2021-11-23 06:11:38 +0000

    Assignment-Operators

---
+++ Assignment-Operators/scripts.js
@@ -0,0 +1,31 @@
+// Assignment Operators
+
+// equal
+var myName = "anuradha";
+console.log(myName);
+
+// Addition with equal
+var age = 24;
+age +=10;
+console.log(age);
+
+// Substarction with equal
+age -=10;
+console.log(age);
+
+// Multiplication with equal
+age *=10;
+console.log(age);
+
+// Devision with equal
+age /=4;
+console.log(age);
+
+// modulus with equal
+age %=13;
+console.log(age);
+
+// Exponentiation with equal
+age **= 2;
+console.log(age);
+
