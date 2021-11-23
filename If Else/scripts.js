7c16ad2b04b9bfcaa38220197b74e50e60bb0e8e
bklanuradha <92729@fhss.sjp.ac.lk>
2021-11-23 08:09:45 +0000

    If-Else

---
+++ If Else/scripts.js
@@ -0,0 +1,19 @@
+// If Else
+
+var a = 10;
+var b = 20;
+
+//  Condition
+if(a > b){
+    console.log(" a is greater than b");
+}
+else if( a < b){
+    console.log("a is less than b");
+}
+else{
+    console.log("a is less than or equal to b");
+}
+
+if(a == 10){
+    console.log("a is equal to 10");
+}
